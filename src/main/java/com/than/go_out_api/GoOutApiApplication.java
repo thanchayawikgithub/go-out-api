f013090832ee7dc785ce5f03eb056cee2466d534
Thxn <155278099+thanchayawikgithub@users.noreply.github.com>
2024-12-09 13:29:24 +0700

    init

---
+++ src/main/java/com/than/go_out_api/GoOutApiApplication.java
@@ -0,0 +1,11 @@
+package com.than.go_out_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoOutApiApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(GoOutApiApplication.class, args);
+  }
+}
---
+++ src/test/java/com/than/go_out_api/GoOutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.than.go_out_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoOutApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
